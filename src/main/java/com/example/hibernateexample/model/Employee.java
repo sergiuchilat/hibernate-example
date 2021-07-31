816a51a8951c79d13bf45a70c4e8cd5d9d50d360
sergiuchilat <chilatsergiu@gmail.com>
2021-07-31 18:01:00 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernateexample/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.hibernateexample.model;
+
+import lombok.Data;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}

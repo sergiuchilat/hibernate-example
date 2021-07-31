a67186ca0f0b5b4db2c8c4ed3dd5adb455e0bdeb
sergiuchilat <chilatsergiu@gmail.com>
2021-07-31 19:57:20 +0300

    Employee model columns configuration

--- src/main/java/com/example/hibernateexample/model/Employee.java
+++ src/main/java/com/example/hibernateexample/model/Employee.java
@@ -13,12 +13,12 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", length = 20, nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", length = 20, nullable = true)
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", length = 100, nullable = false, unique = true)
     private String email;
 }
